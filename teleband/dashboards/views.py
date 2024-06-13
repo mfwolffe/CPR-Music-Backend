59b8a5c160756b1f0dd22d51e3a5df3344743b3c
Michael Stewart <stewarmc@jmu.edu>
2024-06-13 09:53:10 -0400

    this resulted in 1250 SQL queries https://capture.dropbox.com/uiyeHXXMw3iGPJ3w 😩

---
+++ teleband/dashboards/views.py
@@ -0,0 +1,38 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+
+from django.views import generic
+
+from teleband.assignments.models import Assignment
+from teleband.courses.models import Course
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class AssignmentListView(UserPassesTestMixin, generic.ListView):
+    model = Assignment
+
+    def get_queryset(self) -> QuerySet[Any]:
+        results = Assignment.objects.prefetch_related(
+            "piece",
+            "enrollment__course__owner",
+            "instrument",
+            "submissions__attachments",
+            "submissions__grade",
+            "submissions__self_grade",
+        ).all()
+        return results
+
+    # queryset = Course.objects.prefetch_related(
+    #     "enrollment_set__assignment_set__submissions__attachments"
+    # ).all()
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class CourseListView(UserPassesTestMixin, generic.ListView):
+    model = Course
+
+    def test_func(self):
+        return self.request.user.is_superuser
