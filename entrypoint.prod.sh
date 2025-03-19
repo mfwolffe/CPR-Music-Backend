216e6e9b50a7297a4f329c6149eaee2ea4cde621
Adam Vinch <adamvinch@gmail.com>
2025-03-19 13:57:18 -0400

    Specify which settings to use to django

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+export DJANGO_READ_DOT_ENV_FILE=True
+export DJANGO_SETTINGS_MODULE=config.settings.production
+
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 4 config.asgi:application
+python -m gunicorn config.asgi:application \
+        --bind 0.0.0.0:8000 \
+        --workers 4 \
+        -k uvicorn.workers.UvicornWorker
